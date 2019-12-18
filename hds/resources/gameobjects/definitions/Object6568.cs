83037a9a8cb81e47f4d2e9ed2961fd775b234f5f
Alexander Carparelli <neowhoru@gmail.com>
2019-12-18 23:16:50 +0100

    FIX: commit missing stuff

---
+++ hds/resources/gameobjects/definitions/Object6568.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hds
+{
+    public class Object6568 : GameObject
+    {
+        public int SelfViewGroups;
+
+        public Attribute Position = new Attribute(24, "Position");
+        public Attribute HalfExtents = new Attribute(12, "HalfExtents");
+        public Attribute CurrentState = new Attribute(4, "CurrentState");
+        public Attribute Hidden = new Attribute(1, "Hidden");
+        public Attribute Orientation = new Attribute(16, "Orientation");
+
+        public Object6568() : base(5, 2, 10, 7, "Subway Car Default", 0xa819, 0xFFFFFFFF)
+        {
+            this.AddAttribute(ref Position, 0, -1);
+            this.AddAttribute(ref HalfExtents, 1, -1);
+            this.AddAttribute(ref CurrentState, 2, -1);
+            this.AddAttribute(ref Hidden, 3, -1);
+            this.AddAttribute(ref Orientation, 4, -1);
+        }
+    }
+}
---
+++ hds/world/Skill/HyperjumpStep.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hds.world.Skill
+{
+    class JumpStep
+    {
+        public LtVector3f FromPos;
+        public LtVector3f ToPos;
+        public float JumpHeight;
+        public UInt32 endTime;
+        public UInt16 NeededAckSeq;
+        public ushort jumpId;
+        public UInt32 maybeTimeBasedValue;
+
+        public JumpStep(LtVector3f fromPos, LtVector3f toPos, float jumpHeight, uint endTime, ushort jumpId,
+            uint maybeTimeBasedValue)
+        {
+            FromPos = fromPos;
+            ToPos = toPos;
+            JumpHeight = jumpHeight;
+            this.endTime = endTime;
+            this.jumpId = jumpId;
+            this.maybeTimeBasedValue = maybeTimeBasedValue;
+        }
+    }
+}
